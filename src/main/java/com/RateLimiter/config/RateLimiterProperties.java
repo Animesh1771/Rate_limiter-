694a7405b17bae4e08811b3910fe9dc633576692
Animesh <animeshmtb95@gmail.com>
2026-02-08 10:36:22 +0530

    Adding config file

---
+++ src/main/java/com/RateLimiter/config/RateLimiterProperties.java
@@ -0,0 +1,15 @@
+package com.RateLimiter.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "rate-limiter")
+public class RateLimiterProperties {
+    private long capacity=10;
+    private long refillRate=5;
+    private String apiServerUrl="https://localhost:8080";
+    private int timeout=5000;
+}
---
+++ src/main/java/com/RateLimiter/config/RedisProperties.java
@@ -0,0 +1,27 @@
+package com.RateLimiter.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Component
+@Data
+@ConfigurationProperties(prefix="spring.redis")
+public class RedisProperties {
+    private String host="localhost";
+    private int port=6379;
+    private int timeout=2000;
+
+    public JedisPool getJedisPool(){
+        JedisPoolConfig poolConfig =new JedisPoolConfig();
+        poolConfig.setMaxTotal(50);
+        poolConfig.setMaxIdle(10);
+        poolConfig.setMinIdle(5);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        return new JedisPool(poolConfig, host, port, timeout);
+
+    }
+}
